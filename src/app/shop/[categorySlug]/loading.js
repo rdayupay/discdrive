75f7b79fc2919bb9394048ceab41887b1ebb5c18
rdayupay <raqueldayupay@gmail.com>
2024-05-14 21:55:25 +0800

    Add a spinner

--- src/app/shop/[categorySlug]/loading.js
+++ src/app/shop/[categorySlug]/loading.js
@@ -1,7 +1,12 @@
+import Spinner from '@/components/Spinner';
 import React from 'react';
 
 function DiscsLoading() {
-  return <div>Loading...</div>;
+  return (
+    <div>
+      <Spinner />
+    </div>
+  );
 }
 
 export default DiscsLoading;
